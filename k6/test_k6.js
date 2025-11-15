5974f253fd3dd84e1abfff8dc5e66de81328546e
Francisco Oberto Zarazaga <franobertozarazaga@gmail.com>
2025-11-15 03:37:52 -0300

    fix: k6 ingress

--- k6/test_k6.js
+++ k6/test_k6.js
@@ -1,15 +1,38 @@
 import http from 'k6/http';
-import { sleep, check } from 'k6';
+import { sleep, check, fail } from 'k6';
 
 // Configuración del test
 export let options = {
-  vus: 10000,         // 10k usuarios simultáneos
-  duration: '600s', // durante 60 segundos
+  vus: 10,
+  duration: '600s',
 };
 
-// Función principal (lo que cada usuario hace)
+const DNS_TARGET = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local';
+// http.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/app1/ping/');
+
+// ----------------------------------------------
+// Función principal (lo que hace cada usuario VU)
+// ----------------------------------------------
 export default function () {
-  let res = http.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/app1/ping/');
+  let res = http.get(
+    'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/app1/ping/',
+    {
+      headers: {
+        Host: 'localhost'
+      }
+    }
+  );
+
+
+  // Logs útiles por request
+  console.log(`--- REQUEST ---`);
+  console.log(`Status: ${res.status}`);
+  console.log(`Duration: ${res.timings.duration} ms`);
+  console.log(`Body: ${res.body}`);
+  console.log(`Error: ${res.error || "none"}`);
+
+  // Opcional: loggear headers
+  // console.log(`Headers: ${JSON.stringify(res.headers, null, 2)}`);
 
   check(res, {
     'status is 200': (r) => r.status === 200,
